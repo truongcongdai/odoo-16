f183684c0c1d799b72fe638c888a5f672f6d60d7
truongcongdai25@gmail.com <truongcongdai25@gmail.com>
2023-01-10 14:29:36 +0700

    rename customaddons

--- customaddons/exam3/__manifest__.py
+++ customaddons/exam3/__manifest__.py
@@ -9,10 +9,10 @@
     'depends': ['base', 'exam1', 'exam2', 'mail'],
     'data': [
         'security/ir.model.access.csv',
-        'data/sales_purchase_cron.xml',
-        'data/email_template.xml',
-        'views/sales_purchase.xml',
-        'views/s_inherit_hr_department.xml',
+        'data/s_sales_purchase_cron.xml',
+        'data/s_email_template.xml',
+        'views/s_sales_purchase.xml',
+        'views/inherit_hr_department.xml',
         'views/inherit_indicator_evaluation.xml',
 
     ],
