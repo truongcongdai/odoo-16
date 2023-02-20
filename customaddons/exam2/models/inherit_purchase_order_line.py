90e5198038d980abea1a05a3b2f3beb4c85f128d
truongcongdai25@gmail.com <truongcongdai25@gmail.com>
2023-02-20 13:07:01 +0700

    update

--- customaddons/exam2/models/inherit_purchase_order_line.py
+++ customaddons/exam2/models/inherit_purchase_order_line.py
@@ -8,25 +8,6 @@ class InheritPurchaseOrderLine(models.Model):
     product_id = fields.Many2one('product.product', domain=[('purchase_oke', '=', True)], string="Product",
                                  index='btree_not_null')
 
-    # tìm supplier có gía nhỏ nhất
-    # @api.depends('product_id')
-    # def _compute_supplier(self):
-    #     for r in self:
-    #         if r.product_id:
-    #             price_supplier = ''
-    #             # lay ra supplier gia nho nhat
-    #             price = r.product_id.seller_ids.mapped('price')
-    #             if price:
-    #                 min_price = min(r.product_id.seller_ids.mapped('price'))
-    #                 # lay ra tên nhà san xuất co product_id=product_id.id va gia nho nhat
-    #                 price_supplier = r.product_id.seller_ids.search([('price','=',min_price)]).mapped('partner_id.name')
-    #             if price_supplier:
-    #                 # lấy ra tên nhà san xuất có thời gian thấp nhất
-    #                 shortest_delivery_time = r.product_id.seller_ids.search([('price','=',min_price)],order='delay asc', limit=1).partner_id.name
-    #                 r.supplier = shortest_delivery_time
-    #             else:
-    #                 r.supplier = price_supplier
-
     @api.depends('product_id')
     def _compute_supplier(self):
         for rec in self:
