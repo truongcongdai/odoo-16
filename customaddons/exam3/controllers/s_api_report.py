e8129666caa37dec39814449f69156b2b137cb3e
truongcongdai25@gmail.com <truongcongdai25@gmail.com>
2023-02-20 13:06:40 +0700

    update

--- customaddons/exam3/controllers/s_api_report.py
+++ customaddons/exam3/controllers/s_api_report.py
@@ -1,4 +1,5 @@
 import werkzeug.wrappers
+from odoo.exceptions import ValidationError
 
 from odoo import http
 from odoo.http import request
@@ -10,9 +11,11 @@ class ApiReport(http.Controller):
     def controllerapi(self):
         body = json.loads(request.httprequest.data)
         token = "odooneverdie"
-
-        if body["token"] == token and body["month"]:
-
+        if not body.get('token'):
+            raise ValidationError('Chưa có key token trong body!')
+        if not body.get('month'):
+            raise ValidationError('Chưa có key month trong body!')
+        if body.get('token') == token:
             data = {
                 "sales": [],
                 "purchase": [],
@@ -20,21 +23,21 @@ class ApiReport(http.Controller):
             # lấy ra all record của indicator evaluation với supperuser với create_month = month
             indicator_evaluation_record = request.env['indicator.evaluation'].sudo().search(
                 [('month', '=', body["month"])])
-            for i in indicator_evaluation_record:
-                if i:
+            for rec in indicator_evaluation_record:
+                if rec:
                     data['sales'].append({
-                        'name': i.sale_team.name,
-                        'actual_revenue': i.actual_revenue,
-                        'revenue_difference': i.revenue_difference,
+                        'name': rec.sale_team.name,
+                        'actual_revenue': rec.actual_revenue,
+                        'revenue_difference': rec.revenue_difference,
                     })
             # lấy ra all record của hr department với supperuser với create_month = month
             hr_department_record = request.env['hr.department'].sudo().search([('create_month', '=', body["month"])])
-            for i in hr_department_record:
-                if i:
+            for rec in hr_department_record:
+                if rec:
                     data['purchase'].append({
-                        'name': i.name,
-                        'actual_revenue': i.actual_revenue,
-                        'revenue_difference': i.revenue_difference,
+                        'name': rec.name,
+                        'actual_revenue': rec.actual_revenue,
+                        'revenue_difference': rec.revenue_difference,
                     })
 
             return data
